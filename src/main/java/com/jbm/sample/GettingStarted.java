6fec7f725472eaca60d39ee9fbaaafd93470c651
hopeng <haopeng.huang@gmail.com>
2016-09-11 15:34:51 +1000

    added sample

---
+++ src/main/java/com/jbm/sample/GettingStarted.java
@@ -0,0 +1,31 @@
+package com.jbm.sample;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * Created by hopeng on 11/09/2016.
+ */
+public class GettingStarted {
+    public static void main( String[] args ) {
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<Integer, String> customers = hazelcastInstance.getMap( "customers" );
+        customers.put( 1, "Joe" );
+        customers.put( 2, "Ali" );
+        customers.put( 3, "Avi" );
+
+        System.out.println( "Customer with key 1: " + customers.get(1) );
+        System.out.println( "Map Size:" + customers.size() );
+
+        Queue<String> queueCustomers = hazelcastInstance.getQueue( "customers" );
+        queueCustomers.offer( "Tom" );
+        queueCustomers.offer( "Mary" );
+        queueCustomers.offer( "Jane" );
+        System.out.println( "First customer: " + queueCustomers.poll() );
+        System.out.println( "Second customer: "+ queueCustomers.peek() );
+        System.out.println( "Queue size: " + queueCustomers.size() );
+    }
+}
---
+++ src/main/java/com/jbm/sample/GettingStartedClient.java
@@ -0,0 +1,19 @@
+package com.jbm.sample;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+/**
+ * Created by hopeng on 11/09/2016.
+ */
+public class GettingStartedClient {
+    public static void main( String[] args ) {
+        ClientConfig clientConfig = new ClientConfig();
+        HazelcastInstance client = HazelcastClient.newHazelcastClient( clientConfig );
+        IMap map = client.getMap( "customers" );
+        System.out.println( "Map Size:" + map.size() );
+        System.out.println( "Map:" + map.values() );
+    }
+}
